31a07a6404f807cc35641e5650f9d513db011281
Eric Musa <eric.musa17@gmail.com>
2022-02-28 23:51:27 -0500

    added data files to .gitignore and wrote periodic speedtest script

--- down_test.sh
+++ down_test.sh
@@ -1,7 +1,19 @@
 #!/bin/zsh
-
-conda activate speedtest
+source activate speedtest
 datestamp=$(date '+%Y-%m-%d')
+yesterday=$(date -v-1d '+%Y-%m-%d')
 fname="$datestamp.csv"
-echo $datestamp,`speedtest-cli --csv-header` >> $fname
-echo $(date '+%s'),`speedtest-cli --csv` >> $fname
+yname="$yesterday.csv"
+
+if [ -f $fname ]; then
+    if [ -f $yname ]; then
+        echo "AGGREGATING DATA FROM YESTERDAY ($yesterday) <not really, not yet>"
+    fi
+    echo "running speedtest-cli and saving to $fname"
+    echo $(date '+%s'),`speedtest-cli --csv` >> $fname
+else
+    echo "creating $fname and saving speedtest-cli data to it"
+    echo $datestamp,`speedtest-cli --csv-header` >> $fname
+    echo $(date '+%s'),`speedtest-cli --csv` >> $fname
+fi
+echo "finished"
