dafc11b5704e8787f51d2e594681fa2d07191a00
Eric Musa <eric.musa17@gmail.com>
2022-03-02 01:05:02 -0500

    aggregate.py now generates a plot of download and upload speeds with labelled mean and m=1 std bands

--- aggregate.py
+++ aggregate.py
@@ -39,21 +39,15 @@ header = [
     'Share', 
     'IP Address',
 ]
-cols = [i for i, h in enumerate(header) if h != "Share"]
+cols = [i for i, h in enumerate(header) if h not in ["Timestamp", "Share"]]
 ydata = pd.read_csv(csv_template % yesterday_str, names=header, skiprows=1, usecols=cols)
 
-
-def date_parse(date_str, format=None):
-    format = format or datetime_format
-    return _date_parse(date_str).astimezone(pytz.timezone('US/Eastern'))  # .strftime(format)
-
-
 ydata[['Download (Mbps)', 'Upload (Mbps)']] = ydata[['Download (Mbps)', 'Upload (Mbps)']].apply(lambda _: _*1e-6)
-ydata['Timestamp'] = ydata['Timestamp'].apply(date_parse)
+ydata['Datestamp'] = ydata['Datestamp'].apply(lambda _: datetime.fromtimestamp(_))
 
 print(ydata)
 
-shutil.move(csv_template % yesterday, os.path.join('archive', csv_template % yesterday))
+shutil.move(csv_template % yesterday_str, os.path.join('archive', csv_template % yesterday_str))
 
 
 def get_up_low_bounds(values, extend=0.5):
@@ -62,10 +56,10 @@ def get_up_low_bounds(values, extend=0.5):
     center = (mx + mn) / 2
     low = center - rad * (1 + extend)
     up = center + rad * (1 + extend)
-    return [up, low]
+    return [low, up]
 
 
-timestamps = ydata['Timestamp'].values
+datestamps = ydata['Datestamp'].values
 downloads = ydata['Download (Mbps)'].values
 uploads = ydata['Upload (Mbps)'].values
 
@@ -74,12 +68,22 @@ fig, [ax1, ax2] = plt.subplots(2, 1)
 fig.suptitle('Download and Upload Speeds (Mbps) on %s' % today_str)
 
 ax1.set_title('Downloads', loc='left', y=0.01, x=0.01, fontsize='small')
-ax1.plot(timestamps, downloads)
+ax1.plot(datestamps, downloads)
 ax1.set_ylim(get_up_low_bounds(downloads))
+download_mean, download_std = downloads.mean(), downloads.std()
+ax1.hlines(download_mean+download_std, yesterday, today, color="green", label=str(int(download_mean+download_std)))
+ax1.hlines(download_mean, yesterday, today, color="black", label=str(int(download_mean)))
+ax1.hlines(download_mean-download_std, yesterday, today, color="red", label=str(int(download_mean-download_std)))
+ax1.legend(loc="upper left")
 
 ax2.set_title('Uploads', loc='left', y=0.01, x=0.01, fontsize='small')
-ax2.plot(timestamps, uploads)
+ax2.plot(datestamps, uploads)
 ax2.set_ylim(get_up_low_bounds(uploads))
+upload_mean, upload_std = uploads.mean(), uploads.std()
+ax2.hlines(upload_mean+upload_std, yesterday, today, color="green", label=str(int(upload_mean+upload_std)))
+ax2.hlines(upload_mean, yesterday, today, color="black", label=str(int(upload_mean)))
+ax2.hlines(upload_mean-upload_std, yesterday, today, color="red", label=str(int(upload_mean-upload_std)))
+ax2.legend(loc="upper left")
 # fig.show()
 
 import matplotlib.dates as mdates
